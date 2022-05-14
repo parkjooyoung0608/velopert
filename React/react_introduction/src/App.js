222a46fb8b092fe2e824b28298909e69882ba145
jooyoung <mirkduddl@naver.com>
2022-05-14 16:46:42 +0900

    리액트 입문 : JSX 기본 규칙

---
+++ React/react_introduction/src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import Hello from './Hello';
+import './App.css'
+
+function App() {
+  const name = 'react';
+  const style = {
+    backgroundColor:'black',
+    color: 'aqua',
+    fontSize: 24,
+    padding: 24, // 기본단위
+    padding: '1rem' // 다른 단위 사용 시 문자열로 설정
+  }
+  return (
+    <div>
+      {/* 컴포넌트는 일종의 UI 조각, 쉽게 재사용 가능 */}
+      <Hello />
+      <Hello />
+      <Hello />
+      <div style={style}>{name}</div>
+      <div className="gray-box"></div>
+    </div>
+  );
+}
+
+export default App;
