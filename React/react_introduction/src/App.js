612cb006501171f167f8b6ca4178a343a509b52b
jooyoung <mirkduddl@naver.com>
2022-05-14 18:50:48 +0900

    리액트 입문 : 조건부 렌더링

--- React/react_introduction/src/App.js
+++ React/react_introduction/src/App.js
@@ -10,13 +10,18 @@ function App() {
     color: 'aqua',
     fontSize: 24,
     padding: 24, // 기본단위
-    padding: '1rem' // 다른 단위 사용 시 문자열로 설정
+    paddingf: '1rem' // 다른 단위 사용 시 문자열로 설정
   }
   return (
     <div>
       {/* 컴포넌트는 일종의 UI 조각, 쉽게 재사용 가능 */}
       <Wrapper>
-        <Hello name="react" color="red"/>
+        {/* isSpecial props 설정 , 
+          여기서 true는 자바스크립트 값이기 때문에 {}로 닫기
+          true인지 false인지에 따라서 컴포넌트 좌측에 *표시 => 삼항연산자 사용 
+        */}
+        {/* props 이름만 작성하고 값 설정을 생략하면 true로 설정한 것으로 간주 */}
+        <Hello name="react" color="red" isSpecial />
         <Hello color="red"/>
         <Hello />  
       </Wrapper>
