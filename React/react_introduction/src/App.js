6fba899bcd53e0cc0f5aa66b80651c5bd603e213
jooyoung <mirkduddl@naver.com>
2022-05-16 21:59:02 +0900

    리액트 입문 : useCallback을  사용하여 함수 재사용하기

--- React/react_introduction/src/App.js
+++ React/react_introduction/src/App.js
@@ -1,4 +1,4 @@
-import React, { useRef, useState, useMemo } from 'react';
+import React, { useRef, useState, useMemo, useCallback } from 'react';
 import UserList from './UserList';
 import CreateUser from './CreateUser';
 
@@ -7,13 +7,6 @@ function countActiveUsers(users){
   console.log('활성 사용자 수를 세는중...');
   return users.filter(user => user.active).length;
 }
-// active가 true인 사용자의 수를 세어서 화면에 렌더링
-// 콘솔 메세지를 출력한 이유 : 호출될때마다 알수있게 하기 위함
-// 여기서 발생하는 성능적 문제 : input의 값을 바꿀때도 countActiveUsers함수가 호출된다.
-// 활성 사용자 수를 세는건 users에 변화가 있을때만 세어야되는데 input값이 바뀔때도 리렌더링 되므로 
-// 이런 불필요한 호출로 자원이 낭비된다. 이런 상황에서 useMeo라는 hook을 사용해 성능을 최적화 할 수 있다.
-// Memo 는 memoized를 의미하는데, 이는 이전에 계산 한 값을 재사용한다는 의미를 가지고있다.
-
 
 function App() {
   const [Inputs, setInputs] = useState({
@@ -31,6 +24,12 @@ function App() {
     })
   }
 
+  // 주의할 점
+  // 함수 안에 사용하는 상태 혹이 props가 있다면 꼭 deps 배열안에 포함시켜야 된다는 것
+  // 만약에 deps 배열 안에 함수에서 사용하는 값을 넣지 않게 된다면, 함수 내에서 해당 값들을 참조할때 가장 최신 값을 참조할것이라고 보장할 수 없다.
+  // props로 받아온 함수가 있다면 이 도한 deps에 넣어줘야한다.
+  // useMemo를 기반으로 만들어졌다. 다만 함수를 위해 사용할 떄 더욱 편하게 해줄 뿐이다.
+  // useCallback을 사용함으로서 바로 이뤄낼 수 있는 눈에 띄는 최적화는 없다.
   const [users, setUsers] = useState([
     {
       id: 1,
@@ -53,7 +52,7 @@ function App() {
   ]);
 
   const nextId = useRef(4);
-  const onCreate = () => {
+  const onCreate = useCallback(() => {
     const user = {
       id: nextId.current,
       username,
@@ -69,7 +68,14 @@ function App() {
 
 
     nextId.current += 1;
-  }
+  }, [users, username, email]);
+
+  // useCallback 사용하는 이유
+  // onCreate, onRemove, onToggle 함수는 컴포넌트가 리렌더링 될 때마다 새로 만들어진다.
+  // 한번 만든 함수를 필요할 때만 새로 만들고 재사용하는 것은 여전히 중요하다.
+  // props가 바뀌지 않으면 virtual DOM에 새로 렌더링하는 것 조차 하지 않고 컴포넌트의 결과물을 재사용하는 최적화 작업을 할 것이다.
+  // 이 작업을 하려면 함수를 재사용하는것은 필수이다. 
+
 
   const onRemove = id => {
     // user.id가 파라미터로 일치하지 않는 원소만 추출해서 새로운 배열을 만듦
