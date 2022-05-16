41528846eb94a58c300bf5f45cfded9939fe4a58
jooyoung <mirkduddl@naver.com>
2022-05-17 02:07:11 +0900

    리액트 입문 : Immer를 사용한 더 쉬운 불변성 관리

--- React/react_introduction/src/App.js
+++ React/react_introduction/src/App.js
@@ -1,9 +1,14 @@
-import React, { useRef, useReducer, useMemo, useCallback } from 'react';
+import React, { useReducer, useMemo } from 'react';
 import UserList from './UserList';
 import CreateUser from './CreateUser';
-import useInputs from './hooks/useInputs';
+import produce from 'immer';
+// Immer : 상태 업데이트 할 대, 불변성을 신경쓰지 않으면서 업데이트 (Immer)가 불변성 관리를 대신 해준다.
+// 1. Immer 설치 : npm i immer
+// 2. import 
+// 정말 편한 라이브러리지만 성능적으로는 Immer를 사용하지 않은 코드가 좀 더 빠릅니다.
+// 때문에 정말 필요한 곳에만 사용하는걸 권장합니다.
 
-function countActiveUsers(users){
+function countActiveUsers(users) {
   console.log('활성 사용자 수를 세는중...');
   return users.filter(user => user.active).length;
 }
@@ -18,35 +23,36 @@ const initialState = {
     },
     {
       id: 2,
-      username: 'lemon',
-      email: 'lemon.velopert@gmail.com',
+      username: 'tester',
+      email: 'tester@example.com',
       active: false
     },
     {
       id: 3,
-      username: 'jooyoung',
-      email: 'jooyoung.velopert@gmail.com',
+      username: 'liz',
+      email: 'liz@example.com',
       active: false
-    },
+    }
   ]
-}
+};
 
 function reducer(state, action) {
-  switch(action.type){
+  switch (action.type) {
     case 'CREATE_USER':
-      return {
-          users: state.users.concat(action.user)
-        };
+      // 3. produce(수정하고싶은 상태, 업데이트를 정의한느 함수(불변성에 대해 알아서 처리해준다.))
+      return produce(state, draft => {
+        draft.users.push(action.user);
+      });
     case 'TOGGLE_USER':
-      return {
-        users: state.users.map( user => 
-          user.id === action.id ? {...user, active: !user.active} : user
-        )
-      };
+      return produce(state, draft => {
+        const user = draft.users.find(user => user.id === action.id);
+        user.active = !user.active;
+      });
     case 'REMOVE_USER':
-      return { 
-        users: state.users.filter(user => user.id !== action.id)
-      };
+      return produce(state, draft => {
+        const index = draft.users.findIndex(user => user.id === action.id);
+        draft.users.splice(index, 1);
+      });
     default:
       return state;
   }
@@ -54,59 +60,20 @@ function reducer(state, action) {
 
 // UserDispatch 라는 이름으로 내보내줍니다.
 export const UserDispatch = React.createContext(null);
-// 나중에 사용하고 싶을 때 불러서 사용할 수 있습니다.
 
 function App() {
-  const [{ username, email }, onChange, reset] = useInputs ({
-    username: '',
-    email: ''
-  })
   const [state, dispatch] = useReducer(reducer, initialState);
-  const nextId = useRef(4);
 
   const { users } = state;
 
-  const onCreate = useCallback(()=>{
-    dispatch({
-      type: 'CREATE_USER',
-      user: {
-        id: nextId.current,
-        username,
-        email
-      }
-    });
-    reset();
-    nextId.current += 1;
-  }, [username, email]);
-
-  const onToggle = useCallback(id => {
-    dispatch({
-      type: 'TOGGLE_USER',
-      id
-    })
-  }, []);
-
-  const onRemove = useCallback(id => {
-    dispatch({
-      type: 'REMOVE_USER',
-      id
-    });
-  }, []);
-
-  const count = useMemo( () => countActiveUsers(users), [users] )
+  const count = useMemo(() => countActiveUsers(users), [users]);
   return (
-    // UserDispatch Context를 만들어서 어디서든지 dispatch 를 꺼내 쓸 수 있도록 준비
     <UserDispatch.Provider value={dispatch}>
-      <CreateUser
-        username={username} 
-        email={email}
-        onChange={onChange}
-        onCreate={onCreate}
-      />
+      <CreateUser />
       <UserList users={users} />
       <div>활성사용자 수 : {count}</div>
     </UserDispatch.Provider>
-  )
+  );
 }
 
 export default App;
--- React/react_introduction/src/CreateUser.js
+++ React/react_introduction/src/CreateUser.js
@@ -1,27 +1,46 @@
-import React from 'react';
+import React, { useRef, useContext } from 'react';
+import useInputs from './hooks/useInputs';
+import { UserDispatch } from './App';
 
-// React.memo 
-// 컴포넌트 props가 바뀌끼지 않았다면 리렌더링 방지 -> 리렌더링 성능 최적화 
-// 컴포넌트에서 리렌더링이 필요한 상황에서만 리렌더링 하도록 설정
-// 사용법이 굉장히 쉽다. 그냥 감싸주면된다.
-const CreateUser = ({ username, email, onChange, onCreate }) => {
-    return(
-        <div>
-            <input 
-                name='username' 
-                placeholder='계정명' 
-                onChange={onChange} 
-                value={username}
-            />
-            <input 
-                name='email' 
-                placeholder='이메일' 
-                onChange={onChange} 
-                value={email}
-            />
-            <button onClick={onCreate}>등록</button>
-        </div>
-    )
-}
+const CreateUser = () => {
+  const [{ username, email }, onChange, reset] = useInputs({
+    username: '',
+    email: ''
+  });
+
+  const nextId = useRef(4);
+  const dispatch = useContext(UserDispatch);
+
+  const onCreate = () => {
+    dispatch({
+      type: 'CREATE_USER',
+      user: {
+        id: nextId.current,
+        username,
+        email
+      }
+    });
+    reset();
+    nextId.current += 1;
+  };
+
+  return (
+    <div>
+      <input
+        name="username"
+        placeholder="계정명"
+        onChange={onChange}
+        value={username}
+      />
+      <input
+        name="email"
+        placeholder="이메일"
+        onChange={onChange}
+        value={email}
+      />
+      <button onClick={onCreate}>등록</button>
+    </div>
+  );
+};
 
 export default React.memo(CreateUser);
