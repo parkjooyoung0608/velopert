ee871fff7b615d64d1051eb7db750b8aaa807aab
jooyoung <mirkduddl@naver.com>
2022-05-19 01:00:30 +0900

    리액트 입문2: 배열 렌더링하기

---
+++ react2/src/UserList.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+function User({ user }){
+    return (
+        <div>
+            <b>{user.username}</b><span>({user.email})</span>
+        </div>
+    )
+}
+
+const UserList = () => {
+    const users=[
+        {
+            id: 1,
+            username: 'velopert',
+            email: 'mirkduddl@naver.com'
+        },
+        {
+            id: 1,
+            username: 'peddrtz',
+            email: 'rdddl@naver.com'
+        },
+        {
+            id: 1,
+            username: 'opert',
+            email: 'irkdl@naver.com'
+        },
+    ]
+    return (
+        <div>
+            {users.map(user=>(
+                <User user={user} />
+            ))}
+        </div>
+    );
+};
+
+export default UserList;
