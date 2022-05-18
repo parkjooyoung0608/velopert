472075f04b8367cfd08d2a1ca2a0b24cf081fbba
jooyoung <mirkduddl@naver.com>
2022-05-18 22:22:09 +0900

    리액트 입문2: Input 상태 관리하기

--- react2/src/App.js
+++ react2/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
-import Counter from './Counter';
+// import Counter from './Counter';
+import InputSample from './InputSample';
 
 function App() {
   return (
-    <Counter />  
+    <InputSample />  
   );
 }
 
