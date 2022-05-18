84ffe58e6ccb225533569b5c65bdc2543a3a4207
jooyoung <mirkduddl@naver.com>
2022-05-19 00:26:46 +0900

    리액트 입문2: 여러개의 input 상태 관리하기

--- react2/src/InputSample.js
+++ react2/src/InputSample.js
@@ -1,22 +1,38 @@
 import React, {useState} from 'react';
 
 const InputSample = () => {
-    const [text, setText] = useState('');
+    const [inputs, setInpets] = useState({
+        name: '',
+        nickname: '',
+    });
+
+    const {name, nickname} = inputs;
+
 
     const onChange = (e) => {
-        setText(e.target.value);
+        // input 이 onChange 발생하면 키값에 대한 value를 뽑아야한다.
+        const {value, name} = e.target; // target의 name과 value 추출
+        setInpets({
+            ...inputs,
+            [name]:value
+        })
     }
 
     const onReset = () => {
-        setText('');
+        setInpets({
+            name: '',
+            nickname:''
+        })
     }
 
     return (
         <div>
-            <input onChange={onChange} value={text}></input>
+            <input name="name" placeholder='이름' onChange={onChange} value={name}></input>
+            <input name="nickname" placeholder='닉네임' onChange={onChange} value={nickname}></input>
             <button onClick={onReset}>초기화</button>
             <div>
-                <b>값: {text}</b>
+                <b>값: </b>
+                {name} ({nickname})
             </div>
         </div>
     );
